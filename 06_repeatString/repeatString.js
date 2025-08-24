5fa5e2aeffdcf21a66eb59b756a1213001385fec
gabrielpoll <gabrieldarosa.poll@gmail.com>
2025-08-24 17:04:02 -0300

    Create a repeatString function

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
-
-};
+function repeatString(string, quantity) { 
+    let newString = '';
+    if (quantity < 0) {
+        return "ERROR";
+    } else {
+        for (let i = 0; i < quantity; i++) {
+            newString += string;
+        }}
+    return newString;
+}
+console.log(repeatString('hi', -1));
 
 // Do not edit below this line
 module.exports = repeatString;
