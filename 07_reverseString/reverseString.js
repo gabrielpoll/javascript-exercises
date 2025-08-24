80e5f271c57658be5adee7b759ae14fe2f98214e
gabrielpoll <gabrieldarosa.poll@gmail.com>
2025-08-24 17:09:05 -0300

    Create a reverseString function

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,6 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    return str.split("").reverse().join("");
 };
-
+console.log(reverseString("hello my name is gabriel"));
 // Do not edit below this line
 module.exports = reverseString;
