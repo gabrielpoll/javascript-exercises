5102198cb83c3641ebf2e576a2aee10ab6b1d05a
gabrielpoll <gabrieldarosa.poll@gmail.com>
2025-08-25 13:21:05 -0300

    Create leapYears function

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,5 +1,7 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(array, ...args) {
+    return array.filter(val => !args.includes(val));
+}
 
+console.log(removeFromArray([1, 2, 3, 4, 2, 5], 2, 5));
 // Do not edit below this line
 module.exports = removeFromArray;
--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if (a < 0 || b < 0) {
+    return 'ERROR';
+  } else if (Number(a) === a && a % 1 !== 0 || Number(b) === b && b % 1 !== 0) {
+    return 'ERROR';
+  }
+  let sum = 0;
+  if (a < b) {
+    for (let i = a; a < b + 1; a++) {
+        sum += a;
+    }
+  } else {
+    for (let i = b; b < a + 1; b++) {
+        sum += b;
+    }
+  }
+  return sum;
 };
+console.log(sumAll(1, 2.5));
 
 // Do not edit below this line
 module.exports = sumAll;
