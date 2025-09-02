abd5df25c52c1eddd5beae022a320cc29adc21ba
gabrielpoll <gabrieldarosa.poll@gmail.com>
2025-09-02 18:39:19 -0300

    Conclude 12 Calculator exercise

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(numberOne, numberTwo) {
+  return numberOne + numberTwo;
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+	return num1 - num2;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sumItensInArray = array.reduce((accum, currentValue) => accum + currentValue,
+0);
+  return sumItensInArray;
+}
+
+const multiply = function(array) {
+  let getMultiply = array.reduce((accum, item) => accum * item, 
+1);
+  return getMultiply;
+}
+
+
+const power = function(num1, num2) {
+	return num1 ** num2;
 };
 
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(number) {
+  getFactorial = 1
+  if (number === 0 || number === 1) {
+    return 1;
+  } else {
+    for (let i = 1; i <= number; i++) {
+      getFactorial *= i;
+    }
+  }
+  return getFactorial;
 };
 
 // Do not edit below this line
