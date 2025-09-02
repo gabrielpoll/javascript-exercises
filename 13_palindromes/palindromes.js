29c79615c119859fd453a5f930c86d919751cc3a
gabrielpoll <gabrieldarosa.poll@gmail.com>
2025-09-02 19:36:13 -0300

    Create a verify if is a palindrome function

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,6 +1,28 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let getReverseStr = str.split(` `)
+                        .join(``)
+                        .split(``)
+                        .filter(char => /[a-zA-Z0-9]/.test(char))
+                        .reverse()
+                        .join(``)
+                        .toLowerCase();
+    console.log(getReverseStr);
 
+    let getComparasionString = str.split(` `)
+                            .join(``)
+                            .split(``)
+                            .filter(char => /[a-zA-Z0-9]/.test(char))
+                            .join(``)
+                            .toLowerCase();
+    
+    if (getReverseStr === getComparasionString) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+    console.log(palindromes(`r3ace3car`));
 
 // Do not edit below this line
 module.exports = palindromes;
