2665ee8ba2b33fc2be7a950f10564cb6a09b32b2
gabrielpoll <gabrieldarosa.poll@gmail.com>
2026-08-04 13:44:43 -0300

    create a recursive factorial solution

--- computer_science/recursion/1_factorial/factorial.js
+++ computer_science/recursion/1_factorial/factorial.js
@@ -1,5 +1,12 @@
-const factorial = function() {
-
+const factorial = function(n) {
+    if (n <= 1 && n >= 0) {
+        return 1;
+    } else if (n < 0 || n % Math.trunc(n) != 0 || typeof(n) === 'string' || Array.isArray(n)) { 
+        return undefined;
+    } 
+     else {
+        return n * factorial(n-1)
+    }
 };
 
 // Do not edit below this line
