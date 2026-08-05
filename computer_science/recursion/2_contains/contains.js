38556fbd5caa20ae6c507e27cc80c732a8b7ca90
gabrielpoll <gabrieldarosa.poll@gmail.com>
2026-08-05 20:21:05 -0300

    create a contains function that search something inside nested objects

--- computer_science/recursion/2_contains/contains.js
+++ computer_science/recursion/2_contains/contains.js
@@ -1,5 +1,16 @@
-const contains = function() {
-  
+const contains = function(obj, something) {
+  if (typeof(obj) === 'object') {
+    for (const item in obj) {
+      if (Object.is(obj[item], something)) {
+        return true
+      }
+      if (typeof(obj[item]) === 'object') {
+        if (contains(obj[item], something) === true)
+          return true
+      }
+    }
+  return false
+  }
 };
   
 // Do not edit below this line
