2f26346ed2cf6465ae7c428d1d022db6dc8ef4cb
gabrielpoll <gabrieldarosa.poll@gmail.com>
2026-08-05 21:09:20 -0300

    create a function totalInteger that takes a array or obj with nested arrays or obj or both and sum the total of integers inside

--- computer_science/recursion/3_totalIntegers/totalIntegers.js
+++ computer_science/recursion/3_totalIntegers/totalIntegers.js
@@ -1,5 +1,33 @@
-const totalIntegers = function() {
-  
+const totalIntegers = function(obj) {
+  let sumIntegers = 0
+  if (Array.isArray(obj)) {
+    for(let i = 0; i < obj.length; i++) {
+        if (Number.isInteger(obj[i])) {
+            sumIntegers++;
+        }
+        if (Array.isArray(obj[i])) {
+            sumIntegers += totalIntegers(obj[i])
+        }
+        if (!Array.isArray(obj[i]) && typeof(obj[i]) === 'object')
+            sumIntegers += totalIntegers(obj[i])
+    }
+  }
+  if (!Array.isArray(obj) && typeof(obj) === 'object') {
+    for (const item in obj) {
+        if (Number.isInteger(obj[item])) {
+            sumIntegers++
+        }
+        if (!Array.isArray(obj[item]) && typeof(obj[item]) === 'object')
+            sumIntegers += totalIntegers(obj[item])
+        if(Array.isArray(obj[item])) {
+            sumIntegers += totalIntegers(obj[item])
+        }
+    }
+  }
+  if (typeof(obj) != 'object')
+    return undefined
+  return sumIntegers
+
 };
   
 // Do not edit below this line
