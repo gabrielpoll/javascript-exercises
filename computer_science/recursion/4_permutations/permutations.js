39033d25f74cdc15bc91fbd99dc57b2f13fefc40
gabrielpoll <gabrieldarosa.poll@gmail.com>
2026-08-07 13:05:32 -0300

    create a permutations function that use recursion to create all cases for number permutations

--- computer_science/recursion/4_permutations/permutations.js
+++ computer_science/recursion/4_permutations/permutations.js
@@ -1,5 +1,18 @@
-const permutations = function() {
-  
+const permutations = function(array) {
+    if (array.length === 0) {
+        return [array]
+    }
+    let listArray = [];
+    for (let i = 0; i < array.length; i++) {
+        const current = array[i];
+        const remainder = array.filter(item => item !== current);
+        const perms = permutations(remainder);
+        for (permuts of perms) {
+            let final = [current, ...permuts];
+            listArray.push(final);
+        }
+    }
+    return listArray;
 };
   
 // Do not edit below this line
